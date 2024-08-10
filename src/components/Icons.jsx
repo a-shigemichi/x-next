25fc5a801f406ad9c9a1a042102c9dc5045166a0
重道淳志 <shigemichiatsushi@shigemichiatsushinoMacBook-Air.local>
2024-08-10 11:50:10 +0900

    update deletePost

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -55,6 +55,24 @@ export default function Icons({ id, uid }) {
       }, [likes]);
 
 
+      const deletePost = async () => {
+        if (window.confirm('Are you sure you want to delete this post?')) {
+          if (session?.user?.uid === uid) {
+            deleteDoc(doc(db, 'posts', id))
+              .then(() => {
+                console.log('Document successfully deleted!');
+                window.location.reload();
+              })
+              .catch((error) => {
+                console.error('Error removing document: ', error);
+              });
+          } else {
+            alert('You are not authorized to delete this post');
+          }
+        }
+      };
+
+
   return (
     <div className='flex justify-start gap-5 p-2 text-gray-500'>
       <div className='flex items-center'>
@@ -80,9 +98,12 @@ export default function Icons({ id, uid }) {
           </span>
         )}
       </div>
-      <HiOutlineTrash
+      {session?.user?.uid === uid && (
+        <HiOutlineTrash
           className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
+          onClick={deletePost}
         />
+      )}
     
     </div>
   );
